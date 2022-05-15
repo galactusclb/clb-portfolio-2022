283e67501799e21851f41e0c5dd4afe80081909a
galactusclb <chanakabalasuriya123@gmail.com>
2022-05-15 12:02:45 +0530

    Rearrange file structure -2

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-// import style from '@/'
+
+import style from 'assets/styles/modules/home/hero.module.scss'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className={style['clb-main']}>Hero</div>
     )
 }
 
