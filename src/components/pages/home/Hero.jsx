931cf7b8c586d07ad32948b3bbc68879b5a1a788
galactusclb <chanakabalasuriya123@gmail.com>
2022-05-15 20:22:02 +0530

    Add animations to hero section

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { motion } from "framer-motion";
 
 import style from 'assets/styles/modules/home/hero.module.scss'
 
+const banner = {
+    animate: {
+        transition: {
+            delayChildren: 0.4,
+            staggerChildren: 0.1
+        }
+    }
+}
+
+const letterAnimation = {
+    initial: {
+        y: 400,
+    },
+    animate: {
+        y: 0,
+        transition: {
+            ease: [.6, .01, -.05, .95],
+            duration: 1
+        }
+    }
+}
+
 const Hero = () => {
+
+    const [playMarquee, setPlayMarquee] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setPlayMarquee(true);
+        }, 1800)
+    }, []);
+
     return (
-        <div className={style['clb-main']}>
+        <motion.div className={style['clb-main']} variants={banner}>
 
             <div className={`${style['clb-hero-img']}`}>
                 <img src={`${process.env.PUBLIC_URL}/images/propic-800-min.png`} alt="" />
@@ -13,20 +45,80 @@ const Hero = () => {
             <div className="container-fluid">
                 <div className={`${style['clb-hero-center']} row justify-content-end align-items-center`}>
                     <div className="col-lg-6">
-                        <h4>👋Hello, I'm</h4>
-                        <h4>Chanaka Lakshan</h4>
-                        <p>Full-Stack Web Developer</p>
-
-                        <div className={`${style['abuot-me']} row justify-content-center align-items-center mt-5`}>
-                            About me
+                        <div
+                            className=""
+                        >
+                            <motion.h4
+                                initial={{
+                                    opacity: 0,
+                                    y: 80
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    y: 0,
+                                    transition: {
+                                        ease: "easeInOut",
+                                        duration: 1,
+                                        delay: 1.6
+                                    }
+                                }}
+                            >👋Hello, I'm</motion.h4>
+                            <motion.h4
+                                initial={{
+                                    opacity: 0,
+                                    y: 80
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    y: 0,
+                                    transition: {
+                                        ease: "easeInOut",
+                                        duration: 1,
+                                        delay: 1.7
+                                    }
+                                }}
+                            >Chanaka Lakshan</motion.h4>
+                            <motion.p
+                                initial={{
+                                    opacity: 0,
+                                    y: 80
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    y: 0,
+                                    transition: {
+                                        ease: "easeInOut",
+                                        duration: 1,
+                                        delay: 1.8
+                                    }
+                                }}
+                            >Full-Stack Web Developer</motion.p>
                         </div>
+
+                        <motion.div className={`${style['abuot-me']} row justify-content-center align-items-center mt-5`}
+                            initial={{ scale: 0 }}
+                            animate={{ scale: 1 }}
+                            transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 1, delay: 2.2 }}>
+                            <motion.span
+                                className='text-center'
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{
+                                    ease: "easeInOut",
+                                    duration: 1,
+                                    delay: 2.8
+                                }}>
+                                About me
+                            </motion.span>
+                        </motion.div>
                     </div>
                 </div>
 
             </div>
-
-            <BannerRowCenter playMarquee={true} />
-        </div>
+            <div className="">
+                <BannerRowCenter playMarquee={playMarquee} />
+            </div>
+        </motion.div>
     )
 }
 
@@ -44,13 +136,21 @@ const BannerRowCenter = ({ playMarquee }) => {
 };
 
 const AnimatedLetters = ({ title, disabled }) => (
-    <span className={style['row-title']}>
+    <motion.span
+        className={style['row-title']}
+        variants={disabled ? null : banner}
+        initial="initial"
+        animate="animate"
+    >
         {[...title].map((letter) => (
-            <span className={style['row-letter']}>
+            <motion.span
+                className={style['row-letter']}
+                variants={letterAnimation}
+            >
                 {letter}
-            </span>
+            </motion.span>
         ))}
-    </span>
+    </motion.span>
 );
 
 
