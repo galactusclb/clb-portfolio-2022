c124171a0b718b9ce0cc9e7ed3b1742b475fb7ca
galactusclb <chanakabalasuriya123@gmail.com>
2022-07-22 10:51:13 +0530

    add 'Monument Extended' custom font to the app

--- src/layouts/page-loarder/PageLoader.jsx
+++ src/layouts/page-loarder/PageLoader.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import style from 'assets/styles/layouts/page-loarder/page-loader.module.scss'
 import { Transition } from 'libs/providers/TransitionContext';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const PageLoader = ({ EndBackgroundColor = null }) => {
 
@@ -9,28 +10,46 @@ const PageLoader = ({ EndBackgroundColor = null }) => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setPageLoading(false);
-        }, 4500);
+        }, 4800);
         return () => clearTimeout(timer);
     });
 
     return (
         <>
-            <div className={`${style['end-background']} ${EndBackgroundColor == 'white' && style['white']}`}></div>
-            <div className={`${style['loader-wrapper']}`}>
-                <div className={`${style['text-wrapper']}`}>
-                    <div className={`${style['text']} ${style['text-1']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-2']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-3']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-4']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-5']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-6']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-7']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-8']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-9']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-10']}`}>less is more</div>
-                    <div className={`${style['text']} ${style['text-11']}`}>less is more</div>
-                </div>
-            </div>
+            <AnimatePresence>
+                <motion.div initial={{
+                    opacity: 0,
+                    height: "0%",
+                    dur: 4
+                }}
+                    animate={{
+                        opacity: 1,
+                        height: "100vh"
+                    }}
+                    exit={{
+                        // opacity: 0,
+                        height: "0%",
+                        dur: 4
+                    }}>
+                    <div className={`${style['end-background']} ${EndBackgroundColor == 'white' && style['white']}`}></div>
+                    <div className={`${style['loader-wrapper']}`}>
+                        <div className={`${style['text-wrapper']}`}>
+                            <div className={`${style['text']} ${style['text-1']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-2']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-3']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-4']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-5']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-6']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-7']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-8']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-9']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-10']}`}>less is more</div>
+                            <div className={`${style['text']} ${style['text-11']}`}>less is more</div>
+                        </div>
+                    </div>
+
+                </motion.div>
+            </AnimatePresence>
         </>
     )
 }
